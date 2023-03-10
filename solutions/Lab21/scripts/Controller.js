5dafdede991c3b08e278e003ac91f90ffdec29cd
ALowtonQA <alowton@qa.com>
2023-03-10 16:38:06 +0000

    AL: lab 21 solution

---
+++ solutions/Lab21/scripts/Controller.js
@@ -0,0 +1,32 @@
+﻿var myApp = angular.module("myModule", []);
+
+var myController = function ($scope, $http) {
+    $scope.customers = null;
+    $scope.errorText = null;
+    $scope.loading = false;
+    $scope.loadData = function () {
+
+        var successCallback = function (results) {
+            $scope.customers = results.data;
+            $scope.loading = false;
+        }; // end of success
+
+        var failureCallback = function (reason) {
+            $scope.errorText = reason.statusText + "\r\n\r\n" + reason.data;
+            $scope.loading = false;
+        }; // end of Failure
+
+        $scope.customers = null;
+        $scope.errorText = null;
+        $scope.loading = true;
+
+        $http({
+            method: "GET",
+            url: "http://localhost:5000/getcustomers"
+        }).then(successCallback, failureCallback);
+
+    }; // end of loadData
+
+}; // end of myController
+
+myApp.controller("customerController", myController);
